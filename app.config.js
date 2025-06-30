cae643d61e11c10e951de523d26a3f489052de7f
osoba samuel <samuelosoba7@gmail.com>
2025-06-30 17:52:11 +0100

    Updated app.config.js

--- app.config.js
+++ app.config.js
@@ -38,7 +38,7 @@ export default {
       "config": "metro.config.js"
     },
     extra: {
-      EXPO_PUBLIC_API_URL:"https://d7c7-197-211-59-52.ngrok-free.app/api",
+      EXPO_PUBLIC_API_URL:"https://server-samuelosoba-samuels-projects-e534d71a.vercel.app//api",
       EXPO_PUBLIC_REVENUECAT_API_KEY:"goog_OwQUxxbXXDcwGkmUWWwIcWMBaEV",
       eas: {
         projectId: "07fe8602-893a-4994-a6fe-4cc04acafbce"
